0d05cae768d6ae68a8e35c1626d8abf5d94251a4
Akshay Vishwanath Bhosale <bhosaleakshay566@gmail.com>
2023-01-24 19:21:34 +0530

    Update Page404.js

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -4,7 +4,7 @@ import './styles/Page404.css'
 const Page404 = () => {
     return (
         <main className="nofound-container">
-            <h1>Página no encontrada <br/> -Error 404- </h1>
+            <h1>Page not found <br/> -Error 404- </h1>
         </main>
     )
 }
